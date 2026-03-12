1ddfaa7b78600c8e71431fb00c83e0ada575249a
BKH <dallas9115@gmail.com>
2026-03-13 01:59:41 +0900

    docs: application.yml에 CloudFront 도메인 주소 최종 반영

--- src/main/java/com/example/startupproject/member/S3Service.java
+++ src/main/java/com/example/startupproject/member/S3Service.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 import java.io.IOException;
-import java.time.Duration;
 import java.util.UUID;
 
 @Service
@@ -20,12 +16,17 @@ public class S3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
+    // 추가: application.yml에 등록한 CloudFront 도메인 주소
+    @Value("${cloud.aws.cloudfront.domain}")
+    private String cloudFrontDomain;
+
     private final S3Client s3Client;
 
     public S3Service(S3Client s3Client) {
         this.s3Client = s3Client;
     }
 
+    // 1. 파일 업로드는 그대로
     public String uploadFile(MultipartFile file) throws IOException {
         String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
@@ -41,17 +42,9 @@ public class S3Service {
         return fileName;
     }
 
+    // 2. 복잡한 로직을 지우고 단순 주소 조합으로 변경
     public String generatePresignedUrl(String fileName) {
-        try (S3Presigner presigner = S3Presigner.builder()
-                .region(Region.AP_NORTHEAST_2) // 서울 리전 직접 주입
-                .build()) {
-
-            GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
-                    .signatureDuration(Duration.ofMinutes(60)) // 60분 유효기간
-                    .getObjectRequest(req -> req.bucket(bucketName).key(fileName))
-                    .build();
-
-            return presigner.presignGetObject(presignRequest).url().toString();
-        }
+        // 형식: https://dxxxxx.cloudfront.net/파일명
+        return "https://" + cloudFrontDomain + "/" + fileName;
     }
 }
